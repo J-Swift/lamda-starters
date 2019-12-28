507fcc753550e9dfdcdc140201ff5c8d89688dd9
Jimmy Reichley <jimmyqpublik@gmail.com>
2019-12-28 17:23:08 -0500

    Tweak node.zip_it

--- _starter_node/zip_it.sh
+++ _starter_node/zip_it.sh
@@ -5,9 +5,15 @@ set -eu
 main() {
   rm -rf out
   mkdir out
+
+  echo 'installing packages...'
+  npm install >/dev/null 2>&1
+
+  echo 'zipping...'
   zip -r out/lambda.zip index.js node_modules 1>/dev/null
 
-  echo 'Done'
+  echo
+  echo 'Done!'
 }
 
 main
